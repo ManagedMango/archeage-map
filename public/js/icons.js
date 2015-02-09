0ea4b48383d288aae5cb130311cd7ef4d400d79f
Patryk Ptasinski <patryk.ptasinski@ipepe.pl>
2015-02-09 15:32:59 +0100

    Added whole archeage-multi-map project

---
+++ public/js/icons.js
@@ -0,0 +1,342 @@
+﻿		var selectedIcon2 = L.icon({
+			iconUrl:'images/user_icon_1.png',
+			shadowUrl:'images/no_shadow.png',
+			
+			iconSize:[18,18],
+			shadowSize:[18,18],
+			iconAnchor:[10,10],
+			shadowAnchor:[0,0]
+		});
+
+		var selectedIcon3 = L.icon({
+			iconUrl: 'images/user_icon_2.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+			shadowAnchor: [10, 0],  // the same for the shadow
+		});
+		var selectedIcon4 = L.icon({
+			iconUrl: 'images/user_icon_3.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+			shadowAnchor: [10, 0],  // the same for the shadow
+		});
+		var selectedIcon5 = L.icon({
+			iconUrl: 'images/user_icon_4.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+			shadowAnchor: [10, 0],  // the same for the shadow
+		});
+		var selectedIcon6 = L.icon({
+			iconUrl: 'images/user_icon_5.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+			shadowAnchor: [10, 0],  // the same for the shadow
+		});
+		var selectedIcon7 = L.icon({
+			iconUrl: 'images/user_icon_6.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+			shadowAnchor: [10, 0],  // the same for the shadow
+		});		
+		var selectedIcon8 = L.icon({
+			iconUrl: 'images/user_icon_7.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+			shadowAnchor: [10, 0],  // the same for the shadow
+		});		
+		var selectedIcon9 = L.icon({
+			iconUrl: 'images/user_icon_8.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+			shadowAnchor: [10, 0],  // the same for the shadow
+		});		
+		var selectedIcon10 = L.icon({
+			iconUrl: 'images/user_icon_9.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+			shadowAnchor: [10, 0],  // the same for the shadow
+		});		
+		var selectedIcon11 = L.icon({
+			iconUrl: 'images/user_icon_10.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+			shadowAnchor: [10, 0],  // the same for the shadow
+		});		
+		var selectedIcon12 = L.icon({
+			iconUrl: 'images/user_icon_11.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+			shadowAnchor: [10, 0],  // the same for the shadow
+		});
+		var selectedIcon13 = L.icon({
+			iconUrl: 'images/user_icon_12.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
+			shadowAnchor: [10, 0],  // the same for the shadow
+		});
+
+
+		
+		var mineIcon = L.icon({
+			iconUrl: 'images/mine.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [5, 5], // size of the icon
+			shadowSize:   [5, 5], // size of the shadow
+			iconAnchor:   [3, 3], // point of the icon which will correspond to marker's location
+			shadowAnchor: [3, 3],  // the same for the shadow
+		});
+		
+		
+		var treeIcon = L.icon({
+			iconUrl: 'images/tree.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [5, 5], // size of the icon
+			shadowSize:   [5, 5], // size of the shadow
+			iconAnchor:   [3, 3], // point of the icon which will correspond to marker's location
+			shadowAnchor: [3, 3],  // the same for the shadow
+			popupAnchor:  [-1, -3] // point from which the popup should open relative to the iconAnchor
+		});		
+		
+		
+		var animal_res_Icon = L.icon({
+			iconUrl: 'images/animal_ico.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [5, 5], // size of the icon
+			shadowSize:   [5, 5], // size of the shadow
+			iconAnchor:   [3, 3], // point of the icon which will correspond to marker's location
+			shadowAnchor: [3, 3],  // the same for the shadow
+			popupAnchor:  [-1, -3] // point from which the popup should open relative to the iconAnchor
+		});
+
+		
+		var tearIcon = L.icon({
+			iconUrl: 'images/tear.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [16, 16], // size of the icon
+			shadowSize:   [16, 16], // size of the shadow
+			iconAnchor:   [8, 8], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+
+		var publicFarmIcon = L.icon({
+			iconUrl: 'images/public_farm.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [16, 16], // size of the icon
+			shadowSize:   [16, 16], // size of the shadow
+			iconAnchor:   [9, 16], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+		
+		var dungeonIcon = L.icon({
+			iconUrl: 'images/dungeon.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [24, 24], // size of the icon
+			shadowSize:   [24, 24], // size of the shadow
+			iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+		
+		var shipIcon = L.icon({
+			iconUrl: 'images/ship.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [10, 18], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -12] // point from which the popup should open relative to the iconAnchor
+		});
+		
+		var outdoorIcon = L.icon({
+			iconUrl: 'images/skull_red.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [16, 16], // size of the icon
+			shadowSize:   [16, 16], // size of the shadow
+			iconAnchor:   [9, 16], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+		
+		var BusStop = L.icon({
+			iconUrl: 'images/busst.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [24, 24], // size of the icon
+			shadowSize:   [16, 16], // size of the shadow
+			iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+		
+		var SkyStop = L.icon({
+			iconUrl: 'images/skyst.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [24, 24], // size of the icon
+			shadowSize:   [16, 16], // size of the shadow
+			iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+
+		var CityStop = L.icon({
+			iconUrl: 'images/cityst.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [24, 24], // size of the icon
+			shadowSize:   [16, 16], // size of the shadow
+			iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+
+		
+		
+		
+		
+
+
+
+		var wearIcon = L.icon({
+			iconUrl: 'images/wear.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [24, 24], // size of the icon
+			shadowSize:   [24, 24], // size of the shadow
+			iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+		var houseIcon = L.icon({
+			iconUrl: 'images/house.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [24, 24], // size of the icon
+			shadowSize:   [24, 24], // size of the shadow
+			iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+		var sheepIcon = L.icon({
+			iconUrl: 'images/sheep.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [24, 24], // size of the icon
+			shadowSize:   [24, 24], // size of the shadow
+			iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+		var animalIcon = L.icon({
+			iconUrl: 'images/animal.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [24, 24], // size of the icon
+			shadowSize:   [24, 24], // size of the shadow
+			iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+		var vechicleIcon = L.icon({
+			iconUrl: 'images/vechicle.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [24, 24], // size of the icon
+			shadowSize:   [24, 24], // size of the shadow
+			iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+		var glidersIcon = L.icon({
+			iconUrl: 'images/gliders.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [24, 24], // size of the icon
+			shadowSize:   [24, 24], // size of the shadow
+			iconAnchor:   [12, 12], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});		
+		
+		
+		
+		var PvP = L.icon({
+			iconUrl: 'images/freepvp.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [44, 44], // size of the icon
+			shadowSize:   [16, 16], // size of the shadow
+			iconAnchor:   [22, 22], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [-7, -17] // point from which the popup should open relative to the iconAnchor
+		});
+		
+		var timer = L.icon({
+			iconUrl: 'images/timer.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [18, 18], // size of the icon
+			shadowSize:   [18, 18], // size of the shadow
+			iconAnchor:   [9, 9], // point of the icon which will correspond to marker's location
+			shadowAnchor: [18, 18],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
+		
+		
+		
+		
+		
+		
+		var markerIcon = L.icon({
+			iconUrl: 'images/marker.png',
+			shadowUrl: 'images/no_shadow.png',
+
+			iconSize:     [16, 16], // size of the icon
+			shadowSize:   [16, 16], // size of the shadow
+			iconAnchor:   [9, 9], // point of the icon which will correspond to marker's location
+			shadowAnchor: [16, 16],  // the same for the shadow
+			popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+		});
